86c787eb619325133c6ac9ea4ea7ffd620a665c9
Liden <lninyow@gmail.com>
2024-05-11 20:08:51 +0800

    added constructors,getters and setters to startup and user model

--- src/model/startup.model.ts
+++ src/model/startup.model.ts
@@ -46,4 +46,158 @@ export class Startup {
 
 	@Column()
 	views: number;
+
+	constructor(
+		id: number,
+		name: string,
+		description: string,
+		locationLat: number,
+		locationLng: number,
+		locationName: string,
+		founderName: string,
+		websiteUrl: string,
+		logoUrl: string,
+		categories: string,
+		foundedDate: Date,
+		contactInfo: string,
+		likes: number,
+		bookmarks: number,
+		views: number
+	) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.locationLat = locationLat;
+		this.locationLng = locationLng;
+		this.locationName = locationName;
+		this.founderName = founderName;
+		this.websiteUrl = websiteUrl;
+		this.logoUrl = logoUrl;
+		this.categories = categories;
+		this.foundedDate = foundedDate;
+		this.contactInfo = contactInfo;
+		this.likes = likes;
+		this.bookmarks = bookmarks;
+		this.views = views;
+	}
+
+	getId(): number {
+		return this.id;
+	}
+
+	setId(value: number) {
+		this.id = value;
+	}
+
+	getName(): string {
+		return this.name;
+	}
+
+	set(value: string) {
+		this.name = value;
+	}
+
+	getDescription(): string {
+		return this.description;
+	}
+
+	setDescription(value: string) {
+		this.description = value;
+	}
+
+	getLocationLat(): number {
+		return this.locationLat;
+	}
+
+	setLocationLat(value: number) {
+		this.locationLat = value;
+	}
+
+	getLocationLng(): number {
+		return this.locationLng;
+	}
+
+	setLocationLng(value: number) {
+		this.locationLng = value;
+	}
+
+	getLocationName(): string {
+		return this.locationName;
+	}
+
+	setLocationName(value: string) {
+		this.locationName = value;
+	}
+
+	getFounderName(): string {
+		return this.founderName;
+	}
+
+	setFounderName(value: string) {
+		this.founderName = value;
+	}
+
+	getWebsiteUrl(): string {
+		return this.websiteUrl;
+	}
+
+	setWebsiteUrl(value: string) {
+		this.websiteUrl = value;
+	}
+
+	getLogoUrl(): string {
+		return this.logoUrl;
+	}
+
+	setLogoUrl(value: string) {
+		this.logoUrl = value;
+	}
+
+	getCategories(): string {
+		return this.categories;
+	}
+
+	setcategories(value: string) {
+		this.categories = value;
+	}
+
+	getFoundedDate(): Date {
+		return this.foundedDate;
+	}
+
+	setFoundedDate(value: Date) {
+		this.foundedDate = value;
+	}
+
+	getContactInfo(): string {
+		return this.contactInfo;
+	}
+
+	setContactInfo(value: string) {
+		this.contactInfo = value;
+	}
+
+	getLikes(): number {
+		return this.likes;
+	}
+
+	setLikes(value: number) {
+		this.likes = value;
+	}
+
+	getBookmarks(): number {
+		return this.bookmarks;
+	}
+
+	setBookmarks(value: number) {
+		this.bookmarks = value;
+	}
+
+	getViews(): number {
+		return this.views;
+	}
+
+	setViews(value: number) {
+		this.views = value;
+	}
 }
--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -28,4 +28,98 @@ export class User {
 
 	@Column()
 	createdAt: Date;
+
+	constructor(
+		id: number,
+		email: string,
+		hashedPassword: string,
+		avatarUrl: string,
+		firstName: string,
+		lastName: string,
+		location: string,
+		moderator: boolean,
+		createdAt: Date
+	) {
+		this.id = id;
+		this.email = email;
+		this.hashedPassword = hashedPassword;
+		this.avatarUrl = avatarUrl;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.location = location;
+		this.moderator = moderator;
+		this.createdAt = createdAt;
+	}
+
+	getId(): number {
+		return this.id;
+	}
+
+	getEmail(): string {
+		return this.email;
+	}
+
+	getHashedPassword(): string {
+		return this.hashedPassword;
+	}
+
+	getAvatarUrl(): string {
+		return this.avatarUrl;
+	}
+
+	getFirstName(): string {
+		return this.firstName;
+	}
+
+	getLastName(): string {
+		return this.lastName;
+	}
+
+	getLocation(): string {
+		return this.location;
+	}
+
+	isModerator(): boolean {
+		return this.moderator;
+	}
+
+	getCreatedAt(): Date {
+		return this.createdAt;
+	}
+
+	setId(id: number): void {
+		this.id = id;
+	}
+
+	setEmail(email: string): void {
+		this.email = email;
+	}
+
+	setHashedPassword(hashedPassword: string): void {
+		this.hashedPassword = hashedPassword;
+	}
+
+	setAvatarUrl(avatarUrl: string): void {
+		this.avatarUrl = avatarUrl;
+	}
+
+	setFirstName(firstName: string): void {
+		this.firstName = firstName;
+	}
+
+	setLastName(lastName: string): void {
+		this.lastName = lastName;
+	}
+
+	setLocation(location: string): void {
+		this.location = location;
+	}
+
+	setModerator(moderator: boolean): void {
+		this.moderator = moderator;
+	}
+
+	setCreatedAt(createdAt: Date): void {
+		this.createdAt = createdAt;
+	}
 }
