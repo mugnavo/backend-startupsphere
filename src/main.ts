69da1a88065640b53fc9a2b09dacb3c5488a8186
Nathaniel Tampus <nate@mugnavo.com>
2024-05-11 16:17:02 +0800

    update port

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup("docs", app, document);
 
-	await app.listen(3000);
+	await app.listen(parseInt(process.env.PORT) || 3001);
 }
 bootstrap();
