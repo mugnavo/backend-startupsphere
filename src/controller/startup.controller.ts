27e52dba64de5d8bad1b72d4060ddf0646498593
Jameel Marco C. Ursonal <londoholm1@gmail.com>
2024-05-11 22:47:02 +0800

    completed startup controller/service (#4)

--- src/controller/startup.controller.ts
+++ src/controller/startup.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	HttpStatus,
+	Param,
+	Post,
+	Put,
+} from "@nestjs/common";
 import { Startup } from "src/model/startup.model";
 import { StartupService } from "src/service/startup.service";
 
@@ -12,9 +22,27 @@ export class StartupController {
 		return this.startupService.findAll();
 	}
 
+	@HttpCode(HttpStatus.OK)
+	@Get("/:startupId")
+	getOneById(@Param() startupId: number) {
+		return this.startupService.findOneBy(startupId);
+	}
+
 	@HttpCode(HttpStatus.CREATED)
 	@Post("/")
 	create(@Body() startup: Startup) {
 		return this.startupService.create(startup);
+	}
+
+	@HttpCode(HttpStatus.OK)
+	@Put("/:startupId")
+	update(@Param() startupId: number, @Body() startup: Startup) {
+		return this.startupService.update(startupId, startup);
+	}
+
+	@HttpCode(HttpStatus.GONE)
+	@Delete("/:startupId")
+	delete(@Param() startupId: number) {
+		return this.startupService.remove(startupId);
 	}
 }
