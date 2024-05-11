52ccc5bd1929b205f009fb52a03b7766f6d7863d
Nathaniel Tampus <nate@mugnavo.com>
2024-05-11 13:12:46 +0800

    add typeorm & pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		TypeOrmModule.forRoot({
+			type: "postgres",
+			host: process.env.PGHOST,
+			username: process.env.PGUSER,
+			password: process.env.PGPASSWORD,
+			database: process.env.PGDATABASE,
+			synchronize: process.env.NODE_ENV !== "production",
+			entities: [],
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
