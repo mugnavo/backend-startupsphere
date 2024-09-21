77805a87736672e32dadd10a75f11437a78abae8
Nathaniel Tampus <nate@mugnavo.com>
2024-09-21 18:02:08 +0800

    add report model

---
+++ src/model/report.model.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "./user.model";
+
+@Entity()
+export class Report {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@ManyToOne(() => User)
+	@JoinColumn({ name: "user_id" })
+	generated_by: User;
+
+	@Column()
+	name: string;
+
+	@Column()
+	file_type: string;
+
+	@Column()
+	url: string;
+
+	@Column({ type: "timestamp", default: () => "now()" })
+	timestamp: Date;
+}
