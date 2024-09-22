e465cc3c53a21044b4d714f622c5342cc34b5527
Liden <lninyow@gmail.com>
2024-09-22 13:36:07 +0800

    added investor on app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,31 +3,32 @@ import { ConfigModule } from "@nestjs/config";
 import { JwtModule, JwtService } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { DataSource } from "typeorm";
-
 import { AuthGuard } from "./auth/auth.guard";
 import { jwtConstants } from "./auth/constants";
 import { AuthController } from "./controller/auth.controller";
 import { BookmarkController } from "./controller/bookmark.controller";
+import { InvestorController } from "./controller/investor.controller";
 import { LikeController } from "./controller/like.controller";
 import { StartupController } from "./controller/startup.controller";
 import { ViewController } from "./controller/view.controller";
+import { ApiLog } from "./model/apilog.model";
+import { AuditTrail } from "./model/audit.model";
 import { Bookmark } from "./model/bookmark.model";
+import { EntityManage } from "./model/entitymanage.model";
+import { Investor } from "./model/investor.model";
 import { Like } from "./model/like.model";
+import { Metric } from "./model/metric.model";
+import { Report } from "./model/report.model";
 import { Startup } from "./model/startup.model";
 import { User } from "./model/user.model";
 import { View } from "./model/view.model";
 import { AuthService } from "./service/auth.service";
 import { BookmarkService } from "./service/bookmark.service";
+import { InvestorService } from "./service/investor.service";
 import { LikeService } from "./service/like.service";
 import { StartupService } from "./service/startup.service";
 import { UserService } from "./service/user.service";
 import { ViewService } from "./service/view.service";
-import { AuditTrail } from "./model/audit.model";
-import { ApiLog } from "./model/apilog.model";
-import { Metric } from "./model/metric.model";
-import { Report } from "./model/report.model";
-import { Investor } from "./model/investor.model";
-import { EntityManage } from "./model/entitymanage.model";
 
 @Module({
 	imports: [
@@ -57,13 +58,21 @@ import { EntityManage } from "./model/entitymanage.model";
 			signOptions: { expiresIn: "60s" },
 		}),
 	],
-	controllers: [AuthController, StartupController, BookmarkController, LikeController, ViewController],
+	controllers: [
+		AuthController,
+		StartupController,
+		BookmarkController,
+		LikeController,
+		ViewController,
+		InvestorController,
+	],
 	providers: [
 		AuthService,
 		UserService,
 		StartupService,
 		BookmarkService,
 		LikeService,
+		InvestorService,
 		ViewService,
 		JwtService,
 		{
